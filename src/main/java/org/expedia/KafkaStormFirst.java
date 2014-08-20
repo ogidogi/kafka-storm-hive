27de211032828e7bd220a6682f4227bd8cd94e69
EugeneYushin <evgenij.ushin@gmail.com>
2014-08-20 16:30:38 +0300

    Initial creation

---
+++ src/main/java/org/expedia/KafkaStormFirst.java
@@ -0,0 +1,34 @@
+package org.expedia;
+
+import storm.kafka.KafkaSpout;
+import storm.kafka.SpoutConfig;
+import storm.kafka.StringScheme;
+import storm.kafka.ZkHosts;
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.spout.SchemeAsMultiScheme;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.utils.Utils;
+
+public class KafkaStormFirst {
+    public static void main(String[] args) {
+        TopologyBuilder builder = new TopologyBuilder();
+
+        SpoutConfig spoutConf = new SpoutConfig(new ZkHosts("localhost:2181", "/brokers"), "test", "/kafkastorm", "KafkaSpout");
+        spoutConf.scheme = new SchemeAsMultiScheme(new StringScheme());
+        spoutConf.forceFromStart = true;
+
+        builder.setSpout("KafkaSpout", new KafkaSpout(spoutConf), 3);
+        builder.setBolt("KafkaBolt", new PrinterBolt(), 3).shuffleGrouping("KafkaSpout");
+
+        Config conf = new Config();
+        //conf.setDebug(true);
+
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("kafka-test", conf, builder.createTopology());
+
+        Utils.sleep(60000);
+        cluster.shutdown();
+
+    }
+}
---
+++ src/main/java/org/expedia/PrinterBolt.java
@@ -0,0 +1,22 @@
+package org.expedia;
+
+/**
+ * Created by cloudera on 8/20/14.
+ */
+import backtype.storm.topology.BasicOutputCollector;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.tuple.Tuple;
+
+public class PrinterBolt extends BaseBasicBolt {
+
+    @Override
+    public void execute(Tuple tuple, BasicOutputCollector collector) {
+        System.out.println(tuple);
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer ofd) {
+    }
+
+}
