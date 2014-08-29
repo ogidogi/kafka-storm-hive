b6642b833bd02c55e2e12bfd9e97afeba1955acb
EugeneYushin <evgenij.ushin@gmail.com>
2014-08-29 17:40:43 +0300

    Add Trident API, fix to related path

--- src/main/java/org/expedia/test/HDFSSequenceTopology.java
+++ src/main/java/org/expedia/test/HDFSSequenceTopology.java
@@ -1,17 +1,23 @@
 package org.expedia.test;
 
+import backtype.storm.spout.SchemeAsMultiScheme;
 import org.apache.storm.hdfs.common.rotation.MoveFileAction;
 import org.apache.storm.hdfs.trident.HdfsState;
 import org.apache.storm.hdfs.trident.HdfsStateFactory;
 import org.apache.storm.hdfs.trident.HdfsUpdater;
 import org.apache.storm.hdfs.trident.format.DefaultFileNameFormat;
 import org.apache.storm.hdfs.trident.format.DefaultSequenceFormat;
+import org.apache.storm.hdfs.trident.format.DelimitedRecordFormat;
 import org.apache.storm.hdfs.trident.format.FileNameFormat;
 import org.apache.storm.hdfs.trident.rotation.FileRotationPolicy;
 import org.apache.storm.hdfs.trident.rotation.FileSizeRotationPolicy;
 import org.apache.storm.hdfs.trident.sync.CountSyncPolicy;
 import org.apache.storm.hdfs.trident.sync.SyncPolicy;
 
+import storm.kafka.StringScheme;
+import storm.kafka.ZkHosts;
+import storm.kafka.trident.TransactionalTridentKafkaSpout;
+import storm.kafka.trident.TridentKafkaConfig;
 import storm.trident.Stream;
 import storm.trident.TridentTopology;
 import storm.trident.state.StateFactory;
@@ -24,33 +30,38 @@ import backtype.storm.tuple.Values;
 import backtype.storm.utils.Utils;
 
 public class HDFSSequenceTopology {
-    @SuppressWarnings("unchecked")
-    public static StormTopology buildTopology(String hdfsUrl) {
-        FixedBatchSpout spout = new FixedBatchSpout(new Fields("sentence", "key"), 1000, new Values("the cow jumped over the moon", 1l),
-                new Values("the man went to the store and bought some candy", 2l), new Values("four score and seven years ago", 3l),
-                new Values("how many apples can you eat", 4l), new Values("to be or not to be the person", 5l));
-        spout.setCycle(true);
-        TridentTopology topology = new TridentTopology();
-        Stream stream = topology.newStream("spout1", spout);
-        Fields hdfsFields = new Fields("sentence", "key");
-        SyncPolicy syncPolicy = new CountSyncPolicy(2);
-        FileNameFormat fileNameFormat = new DefaultFileNameFormat().withPath("/tmp/hdfs-con").withPrefix("trident").withExtension(".seq");
-        FileRotationPolicy rotationPolicy = new FileSizeRotationPolicy(5.0f, FileSizeRotationPolicy.Units.MB);
-        HdfsState.Options seqOpts = new HdfsState.SequenceFileOptions().withFileNameFormat(fileNameFormat)
-                .withSequenceFormat(new DefaultSequenceFormat("key", "sentence")).withRotationPolicy(rotationPolicy).withFsUrl(hdfsUrl)
-                .addRotationAction(new MoveFileAction().toDestination("/tmp/hdfs-con2/"));
-        StateFactory factory = new HdfsStateFactory().withOptions(seqOpts);
-        stream.partitionPersist(factory, hdfsFields, new HdfsUpdater(), new Fields());
-        return topology.build();
-    }
+        public static StormTopology buildTopology(String hdfsUrl) {
+                TridentKafkaConfig tridentKafkaConfig = new TridentKafkaConfig(new ZkHosts("localhost:2181", "/brokers"), "test_topic");
+                tridentKafkaConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
 
-    public static void main(String[] args) throws Exception {
-        Config conf = new Config();
-        conf.setMaxSpoutPending(5);
-        LocalCluster cluster = new LocalCluster();
-        cluster.submitTopology("wordCounter", conf, buildTopology("hdfs://localhost:8020"));
+                TransactionalTridentKafkaSpout tridentKafkaSpout = new TransactionalTridentKafkaSpout(tridentKafkaConfig);
+                //spout.setCycle(true);
 
-        Utils.sleep(60000);
-        cluster.shutdown();
-    }
+                TridentTopology topology = new TridentTopology();
+
+                Stream stream = topology.newStream("spout1", tridentKafkaSpout);
+                Fields hdfsFields = new Fields("str");
+                //SyncPolicy syncPolicy = new CountSyncPolicy(2);
+                FileNameFormat fileNameFormat = new DefaultFileNameFormat().withPath("/tmp/hdfs-con").withPrefix("trident")
+                        .withExtension(".txt");
+                FileRotationPolicy rotationPolicy = new FileSizeRotationPolicy(5.0f, FileSizeRotationPolicy.Units.MB);
+                HdfsState.Options seqOpts = new HdfsState.HdfsFileOptions().withFileNameFormat(fileNameFormat)
+                        .withRecordFormat(new DelimitedRecordFormat().withFieldDelimiter("|").withFields(new Fields("str")))
+                        .withRotationPolicy(rotationPolicy)
+                        .withFsUrl(hdfsUrl)
+                        .addRotationAction(new MoveFileAction().toDestination("/tmp/hdfs-con2/"));
+                StateFactory factory = new HdfsStateFactory().withOptions(seqOpts);
+                stream.partitionPersist(factory, hdfsFields, new HdfsUpdater(), new Fields());
+                return topology.build();
+        }
+
+        public static void main(String[] args) throws Exception {
+                Config conf = new Config();
+                conf.setMaxSpoutPending(5);
+                LocalCluster cluster = new LocalCluster();
+                cluster.submitTopology("wordCounter", conf, buildTopology("hdfs://localhost:8020"));
+
+                Utils.sleep(180000);
+                cluster.shutdown();
+        }
 }
